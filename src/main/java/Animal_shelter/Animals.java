65a082f334dfbfd4e3bef5ca8f44c774c5ba9c35
Vitaliy <dr.kovalyk2017@yandex.ru>
2024-02-17 17:16:24 +0300

    new class

---
+++ src/main/java/Animal_shelter/Animals.java
@@ -0,0 +1,15 @@
+package Animal_shelter;
+
+abstract class Animals {
+
+    //region Fields
+    protected String type;
+    //endregion
+
+
+    //region Constructor
+    public Animals(String type) {
+        this.type = type;
+    }
+    //endregion
+}
